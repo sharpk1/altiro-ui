cf0797d25fa085735a9b66b6278136be9bc80d68
Kush Shah <kushshah@Kushs-MacBook-Pro.local>
2023-07-27 10:33:02 -0600

    initial commit need api integration

---
+++ src/components/Team.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export const Team = (props) => {
+  return (
+    <div id="team" className="text-center">
+      <div className="container">
+        <div className="col-md-8 col-md-offset-2 section-title">
+          <h2>Meet the Team</h2>
+          <p>Moving Ahead - Growing Organically</p>
+        </div>
+        <div id="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md col-sm-6 team">
+                  <div className="thumbnail">
+                    {" "}
+                    <img
+                      src={d.img}
+                      style={{ height: "319px" }}
+                      alt="..."
+                      className="team-img"
+                    />
+                    <div className="caption">
+                      <h4>{d.name}</h4>
+                      <p>{d.job}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
